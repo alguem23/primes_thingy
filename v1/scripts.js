f6290fbe9059a17630f0bfd23aa685afe49f3097
alguem23 <85797106+alguem23@users.noreply.github.com>
2022-03-20 11:39:28 +0000

    Logs val, type & val+1 of input to console logs decomposed input adds input to decomp_table

--- v1/scripts.js
+++ v1/scripts.js
@@ -1,9 +1,50 @@
-function yup() {
-    document.getElementById("decomp_table").innerHTML += 'Haha'
-}
-
 function ye1() {
     var a = document.getElementById('template')
     a.innerHTML = '<div>God, if this works :)<div>'
     document.getElementById("decomp_table").innerHTML += a.innerHTML
+}
+
+function isPrime(num) {
+    if (! num > 1) return false;
+
+    for (let i in 0..Math.floor(Math.sqrt(num)))
+        if (num % i == 0)
+            return false;
+    
+    return true;
+}
+
+function higherPrime(num) {
+    for (num++; true; num++)
+        if (isPrime(num)) return num;
+}
+
+function decompose(num) {
+    var factor = num
+    var factors = [num]
+    var prime = 2
+    var primes = []
+
+    while (factor != 1) {
+        if (factor % prime != 0) {
+            prime = higherPrime(prime)
+            continue
+        }
+
+        primes.push(prime)
+        factor = Math.floor(factor / prime)
+        factors.push(factor)
+    }
+
+    return [factors, primes]
+}
+
+function decomp() {
+    var a = ~~(document.getElementById('input').value)
+
+    console.log(`Val: ${a}; Type: ${typeof a}; Val + 1: ${a+1}`)
+
+    console.log(decompose(a))
+
+    document.getElementById("decomp_table").innerHTML += a
 }
